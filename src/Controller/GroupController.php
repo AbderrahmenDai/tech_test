8a74ef4f7fbf7a6665ce402d2cbb758ced8eeedd
Abderrahmen Dai <abderrahmen.dai.11@gmail.com>
2023-01-21 11:45:53 +0100

    first commit

---
+++ src/Controller/GroupController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GroupController extends AbstractController
+{
+
+    #[Route('/group', name: 'app_group', methods:'{"GET"}')]
+    public function index(GroupRepository $groupRepository): Response
+    {
+        return $this->render('group/index.html.twig', [
+            'groups' => $groupRepository->findAll(),
+        ]);
+    }
+
+    #[Route("/group/new", name:"group_new", methods:'{"GET","POST"}')]
+    public function new(Request $request): Response
+    {
+        $group = new Group();
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($group);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('group_index');
+        }
+
+        return $this->render('group/new.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+  
+    #[Route("/group/{id}", name:"group_show", methods:'{"GET"}')]
+     
+    public function show(Group $group): Response
+    {
+        return $this->render('group/show.html.twig', [
+            'group' => $group,
+        ]);
+    }
+
+    
+    #[Route("/group/{id}/edit", name:"group_edit", methods:'{"GET","POST"}')]
+    public function edit(Request $request, Group $group): Response
+    {
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('group_index');
+        }
+
+        return $this->render('group/edit.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Group;
+use App\Form\UserType;
+use App\Form\GroupType;
+use App\Repository\UserRepository;
+use App\Repository\GroupRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+
+    }
+
+    
+      #[Route('/user/new', name:"user_new", methods:'{"GET","POST"}')]    
+     public function new(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+     
+    #[Route("/user/{id}", name:"user_show", methods:'{"GET"}')]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    
+    #[Route("/user/{id}/edit", name:"user_edit", methods:'{"GET","POST"}')]
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
